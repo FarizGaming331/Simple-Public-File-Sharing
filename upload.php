eb88e4dec2da85782e035eeaf2b6bdc63b889067
FarizGaming331 <84503739+FarizGaming331@users.noreply.github.com>
2023-11-10 19:04:09 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,16 +3,18 @@
 // ambil data file
 $namaFile = $_FILES['berkas']['name'];
 $namaSementara = $_FILES['berkas']['tmp_name'];
+$serverName = "farizshare";
 
 // tentukan lokasi file akan dipindahkan
 $dirUpload = "uploads/";
 
 // pindahkan file
-$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile+$serverName);
 
 if ($terupload) {
-    echo "Upload Success!<br/>";
+    echo "Upload Successful!<br/>";
     echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+    echo "<a href="./index.html">Back To Uploader</a>";
 } else {
     echo "Upload Failed. Please Try Again.!";
 }
